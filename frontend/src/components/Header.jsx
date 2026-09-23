ef1aaca578a31761ea7e02001d91603fd04eafbe
nsaikiran777 <narlasaikiran65@gmail.com>
2026-09-23 23:55:56 +1000

    Update header text to demonstrate continuous deployment

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -43,7 +43,7 @@ const Header = () => {
             fontWeight: 600,
           }}
         >
-          KoalaTech University
+          KoalaTech University - Deployed by CD
         </Typography>
 
         <Box
